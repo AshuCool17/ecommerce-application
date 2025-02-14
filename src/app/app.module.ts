bcdd82ec363473dd3ad8947b5de6a6d8152aee4a
Ashutosh Mahato <ashutosh.mahato@gmail.com>
2025-02-14 09:46:22 +0530

    Added Browser & approuting module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,5 +3,9 @@ import { AppComponent } from "./app.component";
 @NgModule({
     declarations: [
         AppComponent
-    ]
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
 });
