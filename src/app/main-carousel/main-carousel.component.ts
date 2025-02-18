c3d3233999b05e287f224d1047d8eae6f0880e38
Ashutosh Mahato <ashutosh.mahato@gmail.com>
2025-02-18 09:30:25 +0530

    Added main carousel ts

---
+++ src/app/main-carousel/main-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-carousel.component.html',
+  styleUrl: './main-carousel.component.scss'
+})
+export class MainCarouselComponent {
+
+}
